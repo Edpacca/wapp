866d3221811b286e3d6ccba3ed17678a6216a557
Eddie Pace <epace@scottlogic.com>
2021-11-19 11:34:06 +0000

    secure api with authO

--- server/src/routes/controllers/userController.ts
+++ server/src/routes/controllers/userController.ts
@@ -1,38 +1,121 @@
-import { Error, NativeError } from 'mongoose'
-import User from '../../models/userModel'
+import User from '../../models/userModel';
+import { v4 as uuid } from 'uuid';
 
-export function CreateUser(req: any, res: any): any {
-    const body = req.body
+export async function CreateUser(request, result) {
 
-    if (!body) {
-        return res.status(400).json({
-            success: false,
-            error: 'Provide user data'
-        })
+    const user = await User.findOne({family: request.body.family});
+    let familyId;
+    if (user) {
+        familyId = user.familyId
+    } else {
+        familyId = uuid();
     }
 
-    const user = new User(body)
+    try {
+        const user = new User({
+            name: request.body.name,
+            family: request.body.family,
+            familyId: familyId,
+            passcode: request.body.passcode,
+            starter: null,
+            main: null,
+            dessert: null,
+            diet: null
+        });
 
-    if (!user) {
-        return res.status(400).json({
-            success: false,
-            error: 'invalid user model provided'
-        })
-    }
-
-    user
+        user
         .save()
         .then(() => {
-            return res.status(201).json({
+            return result.status(201).json({
                 success: true,
                 id: user._id,
                 message: `Create user ${user.name} of ${user.family} group`
-            })
+            });
+        });
+    } catch (error) {
+        return result.status(400).json({
+            error,
+            message: 'User not created',
         })
-        .catch(error => {
-            return res.status(400).json({
-                error,
-                message: 'User not created',
+    }
+}
+
+export async function GetUser(request, result) {
+
+    User.findById({_id: request.query.id}, (err, user) => {
+        
+        if (!user) {
+            return result.status(404).json({
+                success: false,
+                error: 'User not found',
             })
+        }
+
+        return result.status(200).json({
+            id: user._id,
+            name: user.name,
+            family: user.family,
+            starter: user.starter,
+            main: user.main,
+            dessert: user.dessert
         })
+    })
+}
+
+export function GetUsers(request, result) {
+
+    User.find({}, function(err, users) {
+        return result.status(200).json(users)
+    })
+}
+
+export async function GetUsersByFamily(request, result) {
+    const users = await User.find({ 'family' : request.body.family})
+
+    if (!users || users.length === 0){
+        return result.status(400).json({
+            success: false,
+            error: 'No family found'
+        })
+    }
+
+    return result.status(200).json(users);
+}
+
+export async function UpdateMealChoices(request, result) {
+
+    const body = request.body
+
+    if (!body) {
+        return result.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
+
+    console.log(body);
+
+    User.findByIdAndUpdate({_id: request.query.id}, { "starter": body.starter, "main": body.main, "dessert": body.dessert, "diet": body.diet }, (err, user) => {
+        
+        if (!user) {
+            return result.status(404).json({
+                success: false,
+                error: 'User not found',
+            })
+        }
+
+        if (err) {
+            return result.status(500).send(err)
+        }
+
+        return result.status(200).json({
+            success: true,
+            id: user._id,
+            message: `meal choices updated for ${user.name}`,
+            starter: body.starter,
+            main: body.main,
+            dessert: body.dessert,
+            diet: body.diet
+        })
+    })
 }
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,16 +1,31 @@
 import * as _ from 'lodash';
 import * as express from 'express';
-import { CreateUser } from './controllers/userController';
+import { CreateUser, GetUser, GetUsers, GetUsersByFamily, UpdateMealChoices } from './controllers/userController';
 
 export function appRouter(app: express.Express): void {
 
     // GET dinner
     app.get("/dinner", (request, result) => {
-        result.status(200);
-        result.json("pork chops yo");
+        result.status(200).send("pork chops yo");
     });
+
+    app.put("/dinner", (request, result) => {
+        return UpdateMealChoices(request, result);
+    })
 
     app.post("/user", (request, result) => {
         return CreateUser(request, result);
+    })
+
+    app.get("/user", (request, result) => {
+        return GetUser(request, result);
+    })
+
+    app.get("/user/all", (request, result) => {
+        return GetUsers(request, result);
+    })
+
+    app.get("/user/family", (request, result) => {
+        return GetUsersByFamily(request, result);
     })
 }
