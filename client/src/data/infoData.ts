c9fbc181948bed86a67cfed7fb56d0d9e5526f01
Edpacca <eddie@pace.me.uk>
2021-12-04 13:43:56 +0000

    add info data

---
+++ client/src/data/infoData.ts
@@ -0,0 +1,5 @@
+const dressCode: string = "We don't expect black tie but dress classy folks! We're going with the theme of woodlands and nature - loosely going with earth colours like dark green and brown. But most importantly we want you to feel fabulous in whatever you choose to wear.";
+const accommodation:
+
+
+export { dressCode }
