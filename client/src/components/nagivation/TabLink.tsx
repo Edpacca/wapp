f8b6aa5a2c825575febf8f27cad70509810dce44
Eddie Pace <epace@scottlogic.com>
2022-02-11 17:50:32 +0000

    css navbar

--- client/src/components/nagivation/TabLink.tsx
+++ client/src/components/nagivation/TabLink.tsx
@@ -1,8 +1,7 @@
 export function TabLink(props: {title: string, isActive: boolean, onClick: () => void}) {
     return(
-        <div className={`tabLink ${props.isActive ? "activeLink" : ""}`}>
-            <p onClick={props.onClick}
-            >{props.title}</p>
+        <div className={`tabLink ${props.isActive ? "activeLink" : ""}`} onClick={props.onClick}>
+            <p className='tabText'>{props.title}</p>
         </div>
     )
 }
