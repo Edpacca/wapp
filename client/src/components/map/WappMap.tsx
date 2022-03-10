cf7ba7cb32dda1ae2a9b66aa823ee4bb3f1eb56f
Edpacca <eddie@pace.me.uk>
2022-03-10 00:15:31 +0000

    map complete

--- client/src/components/map/WappMap.tsx
+++ client/src/components/map/WappMap.tsx
@@ -1,26 +1,52 @@
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-import { useCallback, useRef } from 'react';
-import { mapStyle } from './mapStyle';
-import house from '../../assets/icons/house.svg';
+import { GoogleMap, useLoadScript, Marker, Polyline } from '@react-google-maps/api';
+import { useCallback, useRef, useState } from 'react';
+import lochay from '../../assets/map-icons/lochay.svg';
+import finlarig from '../../assets/map-icons/finlarig.svg';
+import registry from '../../assets/map-icons/registry.svg';
+import footsteps from '../../assets/map-icons/footsteps.svg';
+import { ACHARN_WOODS } from '../../data/acharnWoods';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 const containerStyle = {
   width: '90%',
-  height: '80vh'
+  height: '85vh',
+  border: '2px solid yellowgreen',
+  borderRadius: '2vw',
 };
 
-const houseCoords = { lat: 56.47815940703385, lng: -4.324116001400637 };
-const finlarigCoords = {lat: 56.47505699164878, lng: -4.314898901371605}
+const lochayCoords = { lat: 56.47815940703385, lng: -4.324116001400637 };
+const finlarigCoords = { lat: 56.47505699164878, lng: -4.314898901371605 };
+const registryCoords = { lat: 56.46795190718324, lng: -4.318373729857583 };
+const walkCoords = { lat: 56.45960546129408, lng: -4.320894272525618 };
+const center = { lat: 56.46995190718324, lng: -4.322373729857583 };
 
 const libraries: ["places"] = ["places"];
+const id = 'ed641887d0190883';
 
 const options = {
-  styles: mapStyle,
+  mapId: id,
   disableDefaultUI: true,
   zoomControl: true,
 }
 
+const lineOptions = {
+  strokeColor: '#d9ff00',
+  strokeOpacity: 1,
+  strokeWeight: 8,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+  radius: 30000,
+  paths: ACHARN_WOODS,
+  zIndex: 1
+}
+
+
 export function WappMap() {
 
+  const [isMarkersShowing, setIsMarkersShowing] = useState(true);
   const mapRef = useRef();
   const onMapLoad = useCallback((map) => {
     mapRef.current = map;
@@ -29,29 +55,77 @@ export function WappMap() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY as string,
     libraries,
+    
+    mapIds: [id]
   })
 
   if (loadError) return <p>Error loading maps</p>
   if (!isLoaded) return <p>Loading maps</p>
+  
+    const lochayIcon: google.maps.Icon = {
+      url: lochay,
+      scaledSize: new google.maps.Size(120, 120),
+    }
+
+    const lochayLabel: google.maps.MarkerLabel = {
+      text: "Reception",
+      className: "map-label",
+      color: "white"
+    }
+
+    const finlarigIcon: google.maps.Icon = {
+      url: finlarig,
+      scaledSize: new google.maps.Size(100, 100),
+    }
+
+    const finlarigLabel: google.maps.MarkerLabel = {
+      text: "Ceremony",
+      className: "map-label",
+      color: "white"
+    }
+
+    const registryIcon: google.maps.Icon = {
+      url: registry,
+      scaledSize: new google.maps.Size(70, 70),
+    }
+
+    const registryLabel: google.maps.MarkerLabel = {
+      text: "Registry Office",
+      className: "map-label",
+      color: "white"
+    }
+
+    const walkIcon: google.maps.Icon = {
+      url: footsteps,
+      scaledSize: new google.maps.Size(100, 100),
+    }
+
+    const walkLabel: google.maps.MarkerLabel = {
+      text: "Sunday walk",
+      className: "map-label",
+      color: "white"
+    }
 
     return (
-    <div className="App-header">
+    <div className="map-wrap">
+      <button className="map-button" onClick={() => setIsMarkersShowing(!isMarkersShowing)}><FontAwesomeIcon icon={faEye}/></button>
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={houseCoords}
-        zoom={15}
+        center={center}
+        zoom={14.4}
         options={options}
         onLoad={onMapLoad}
       >
-        { 
-        <div>
-          <Marker icon={house} key={"house"} position={houseCoords}/>
-          <Marker key={"castle"} position={finlarigCoords}/>
-        </div>
+        { isMarkersShowing && 
+        <>
+          <Marker label={lochayLabel} icon={lochayIcon} key={"house"} position={lochayCoords}/>
+          <Marker label={finlarigLabel} icon={finlarigIcon} key={"castle"} position={finlarigCoords}/>
+          <Marker label={registryLabel} icon={registryIcon} key={"registry"} position={registryCoords}/>
+          <Marker label={walkLabel} icon={walkIcon} key={"walk"} position={walkCoords}/>
+          <Polyline path={ACHARN_WOODS} options={lineOptions} />
+        </>
         }
-        <></>
       </GoogleMap>
     </div>
-     
   )
 }
