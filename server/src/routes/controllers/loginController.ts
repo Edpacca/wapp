8dd29a31bc579e2a07ca57a090b9568c391abb23
Edpacca <eddie@pace.me.uk>
2022-03-11 19:17:22 +0000

    map tweaks and selective room show

--- server/src/routes/controllers/loginController.ts
+++ server/src/routes/controllers/loginController.ts
@@ -28,7 +28,8 @@ export async function authenticate(request, result) {
                     const user = await User.findOne({family: name});
                     const guests = await getGuestObjectByFamily(name);
                     const seats = await getSeats();
-                    const rooms = await getRooms();
+                    const guestsWithRooms = guests.filter(guest => guest.room != null).length;
+                    const rooms = guestsWithRooms > 0 ? await getRooms() : [];
                     const arrivals = await getArrivals();
                     const userResponse: UserResponse = {
                         id: user._id,
