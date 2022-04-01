8050af15ef2342197b5c8068fd0ae9f815acf06d
Eddie Pace <epace@scottlogic.com>
2022-04-01 15:12:33 +0100

    handle errors

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -14,20 +14,24 @@ function AuthContextProvider(props: any) {
     const dispatch = useAppDispatch();
 
     async function authenticateSession() {
-        const loggedIn = await fetch(`${process.env.REACT_APP_EXPRESS_SERVER}/authenticate`, {
-            credentials: 'include',
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-access-token' : `${process.env.REACT_APP_CLIENT_TOKEN}`
+        try {
+            const loggedIn = await fetch(`${process.env.REACT_APP_EXPRESS_SERVER}/authenticate`, {
+                credentials: 'include',
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token' : `${process.env.REACT_APP_CLIENT_TOKEN}`
+                }
+            }).then(response => response.json());
+    
+            setLoginContext(loggedIn.type);
+    
+            if (loggedIn.type === 'user') {
+                dispatch({ type: 'users/loginRefresh', payload: loggedIn.data });
             }
-        }).then(response => response.json());
-
-        setLoginContext(loggedIn.type);
-
-        if (loggedIn.type === 'user') {
-            dispatch({ type: 'users/loginRefresh', payload: loggedIn.data });
+        } catch (error) {
+            console.log("Failed to connect to server");
         }
     }
 
