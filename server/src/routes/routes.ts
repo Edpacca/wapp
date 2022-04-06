61ad3eed2c072fe30f986832a66cd483fac6be3e
Edpacca <eddie@pace.me.uk>
2022-04-06 18:01:34 +0100

    morgan logging

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -4,86 +4,67 @@ import { batchUpdateGuests, createGuest as createGuest, getGuest as getGuest, ge
 import { addGuestToFamily, registerUser as registerUser } from './controllers/usersController';
 import { authenticate, logout, loginUser, loginAdmin } from './controllers/loginController';
 import { verifyAdminToken, verifyClientToken, verifyUserToken } from '../middleware/auth';
-import { registerAdmin } from './controllers/adminController';
 import { getArrivalsAdmin, updateArrival } from './controllers/arrivalController';
 
 export function appRouter(app: express.Express): void {
 
     // AUTHENTICATION
     app.get("/authenticate", (request, result) => {
-        console.log(request.body);
         return authenticate(request, result);
     });
 
     app.post("/login", verifyClientToken, (request, result) => {
-        console.log(request.body);
         return loginUser(request, result);
     });
 
     app.post("/admin/login", verifyClientToken, (request, result) => {
-        console.log(request.body);
         return loginAdmin(request, result);
     });
 
     app.post("/logout", verifyClientToken, (request, result) => {
-        console.log(request.body);
         return logout(request, result);
     });
 
     // USER
     app.post("/guest", verifyUserToken, (request, result) => {
-        console.log(request.body);
         return createGuest(request, result);
     })
 
     app.get("/guest", verifyUserToken, (request, result) => {
-        console.log(request.body);
         return getGuest(request, result);
     })
 
     app.get("/guest/family", verifyUserToken, (request, result) => {
-        console.log(request.body);
         return getGuestsByFamily(request, result);
     })
 
     app.put("/guest", verifyUserToken, (request, result) => {
-        console.log(request.body);
         return updateMealChoices(request, result);
     })
 
     app.put("/guest/arrival", verifyUserToken, (request, result) => {
-        console.log(request.body);
         return updateArrival(request, result);
     })
 
     // ADMIN
     app.get("/guest/all", verifyAdminToken, (request, result) => {
-        console.log(request.body);
         return getGuests(request, result);
     })
 
     app.get("/guest/arrival", verifyAdminToken, (request, result) => {
-        console.log(request.body);
         return getArrivalsAdmin(request, result);
     })
 
     app.put("/guest/all", verifyAdminToken, (request, result) => {
-        console.log(request.body);
         return batchUpdateGuests(request, result);
     })
 
     app.post("/register/user", verifyAdminToken, (request, result) => {
-        console.log(request.body);
         return registerUser(request, result);
     });
 
     app.post("/register/guest", verifyAdminToken, (request, result) => {
-        console.log(request.body);
         return addGuestToFamily(request, result);
     });
-
-    // app.post("/admin/register", verifyAdminToken, (request, result) => {
-    //     return result.status(200).send(registerAdmin());
-    // });
 }
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,24 +6,25 @@ import * as _ from 'lodash';
 import cors from 'cors';
 import db from '../db'
 import { appRouter as routes } from './routes/routes';
-import 'dotenv/config';
 import { registerAdmin } from './routes/controllers/adminController';
+import 'dotenv/config';
+
+morgan.token('body', (req, res) => [JSON.stringify(req.body)]);
 
 const app = express();
-const { API_PORT } = process.env;
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({credentials: true, origin: [`${process.env.CLIENT_URL}`]}));
 app.use(morgan('dev'));
-
+app.use(morgan(':body \n'));
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 routes(app);
 
-const server = app.listen(API_PORT, function () {
+const server = app.listen(process.env.API_PORT, function () {
     registerAdmin();
     const address = server?.address();
     const port = _.isString(address) ? address : address?.port;
